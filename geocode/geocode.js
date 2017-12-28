3477fcbf38e61fca3c61637325a870cf7849bfa5
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2017-12-28 11:46:19 +0000

    Add Handle output when to many requests done

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -8,13 +8,15 @@ var geocodeAddress = (address, callback) => {
     if (error){
       callback('Could not get response from GOOGLE')
     }else if (body.status === 'ZERO_RESULTS'){
-      callback('Unable to find that address:', address)
+      callback(`Unable to find that address: ${address}`)
     }else if (body.status === 'OK') {
       callback(undefined, {
         address: body.results[0].formatted_address,
         latitude: body.results[0].geometry.location.lat,
         longitude: body.results[0].geometry.location.lng
       })
+    }else {
+      callback('Too many requests to Google server was made \n try again later')
     }
   });
 }
