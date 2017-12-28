74392455d48587c43eb0dc3b888e4bb0ef24dae3
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2017-12-28 14:40:06 +0000

    Replace abstract callbacks with js promises

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,25 +1,27 @@
 const request = require('request');
 
-var geocodeAddress = (address, callback) => {
-  request({
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
-    json: true
-  }, (error, response, body) => {
-    if (error){
-      callback('Could not get response from GOOGLE')
-    }else if (body.status === 'ZERO_RESULTS'){
-      callback(`Unable to find that address: ${address}`)
-    }else if (body.status === 'OK') {
-      callback(undefined, {
-        address: body.results[0].formatted_address,
-        latitude: body.results[0].geometry.location.lat,
-        longitude: body.results[0].geometry.location.lng
-      })
-    }else {
-      callback('Too many requests to Google server was made \n try again later')
-    }
-  });
-}
+var geocodeAddress = (address) => {
+   return new Promise((resolve, reject) => {
+     request({
+       url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+       json: true
+     }, (error, response, body) => {
+       if (error){
+         reject('Could not get response from GOOGLE')
+       }else if (body.status === 'ZERO_RESULTS'){
+         reject(`Unable to find that address: ${address}`)
+       }else if (body.status === 'OK') {
+         resolve({
+           address: body.results[0].formatted_address,
+           latitude: body.results[0].geometry.location.lat,
+           longitude: body.results[0].geometry.location.lng
+         })
+       }else {
+         reject('Too many requests to Google server was made \n try again later')
+       }
+     });
+   });
+};
 
 module.exports = {
   geocodeAddress
